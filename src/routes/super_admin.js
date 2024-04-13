34c36b5e8a31526b37f280af4f90410dfa444cf2
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2024-04-14 00:58:02 +0530

    added password Hashing and email validation

--- src/routes/super_admin.js
+++ src/routes/super_admin.js
@@ -1,21 +1,56 @@
 const express = require("express");
-// const bcrypt = require('bcryptjs');
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const db = require("../db/config");
+const validator = require("validator");
 
-// -----------------------------------------------------add new super admin ---------------------------------------------
-//http://localhost:4000/super_admin/add_admin
-router.post("/add_admin", async (req, res) => {
+// -----------------------------------------------------signup new super admin ---------------------------------------------
+//http://localhost:4000/super_admin/signup
+router.post("/signup", async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const { name, email} = req.body;
     const admin_id = parseInt(Math.random() * 100000);
-    const response =
+     // Validate email format
+     if (!validator.isEmail(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }else{
+      const adminCheck = await db.query(`select * from super_admin where email = '${email}'`);
+    if(adminCheck.rowCount>0){
+      res.status(400).json({result: "Admin with this Email Already exists"});
+    }else{
+      const response =
       await db.query(`INSERT INTO super_admin (admin_id,name,email,password )
-          VALUES ('${admin_id}',' ${name}','${email}','${password}') on conflict do nothing`);
+          VALUES ('${admin_id}',' ${name}','${email}','${hashedPassword}') on conflict do nothing`);
 
     res.status(201).json(response);
+    }
+    }
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+// ---------------------------------------------------login super admin-----------------------------------------------
+//http://localhost:4000/super_admin/login
+router.post("/login", async (req, res) => {
+  const { email, password, name } = req.body;
+  const payload = {name:name, email: email, password: password };
+  const user = await db.query(`select * from super_admin where email = '${email}'`);
+  if (user.rowCount == 0) {
+    return res.status(400).json({result:"cannot find admin with this email"});
+  }
+  try {
+    
+    if (await bcrypt.compare(req.body.password, user.rows[0].password)){
+      res.status(200).json(payload);
+    }
+   else{
+    res.status(400).json({result:"wrong password"});
+    }
+  } catch (err) {
+    res.status(401).json(err);
   }
 });
 
@@ -51,9 +86,11 @@ router.get("/:admin_id", async (req, res) => {
 router.put("/:admin_id", async (req, res) => {
   try {
     const admin_id = req.params.admin_id;
-    const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const { name, email } = req.body;
     const response = await db.query(
-      `UPDATE super_admin SET name = '${name}', email = '${email}', password = '${password}' WHERE admin_id = ${admin_id}`
+      `UPDATE super_admin SET name = '${name}', email = '${email}', password = '${hashedPassword}' WHERE admin_id = ${admin_id}`
     );
 
     res.status(200).json(response.rows);
